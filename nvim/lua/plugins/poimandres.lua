09a5ce9048c429a75d9ffd50e92f5ad7c6ad7e01
gurditt singh <gurdittsingh1508@gmail.com>
2024-12-05 20:39:28 +0530

    batman

---
+++ nvim/lua/plugins/poimandres.lua
@@ -0,0 +1,12 @@
+-- return {
+--   "olivercederborg/poimandres.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd("colorscheme poimandres")
+--   end,
+--   setup = {
+--     disable_background = true,
+--   },
+-- }
+return {}
---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,21 @@
+-- return {
+--
+--   {
+--     "folke/tokyonight.nvim",
+--     -- opts = {
+--     --   transparent = true,
+--     --   styles = {
+--     --     sidebars = "transparent",
+--     --     float = "transparent",
+--     --   },
+--     -- },
+--   },
+--   -- Configure lazyvim to load theme
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "tokyonight",
+--     },
+--   },
+-- }
+return {}
